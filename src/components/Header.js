116cf561eca705c60bf29a30dbda55cba8af0326
Piotr Ptak <robakzdobywca@gmail.com>
2018-05-26 23:46:35 +0200

    :art: platform specific styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, StyleSheet } from 'react-native';
 
-const styles = {
+const styles = StyleSheet.create({
   viewStyle: {
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
     paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2},
-    shadowOpacity: 0.2
+    ...Platform.select({
+      ios: {
+        shadowOffset:{  width: 10,  height: 10,  },
+        shadowColor: 'black',
+        shadowOpacity: 1.0
+      },
+      android: {
+        elevation: 4
+      }
+    })
   },
   textStyle: {
     fontSize: 20,
     color: '#000'
   }
-};
+});
 
 export default class Header extends Component {
   constructor(props) {
